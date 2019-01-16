df930535c17df943b6deb213327699c88026634e
darrenBlanckensee <darren@entrostat.com>
2019-01-16 11:08:41 +0200

    Added heroku functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -8,7 +8,7 @@ publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-// 
+//
 // app.get('/', (req, res) => {
 //   res.render(publicPath);
 // });
