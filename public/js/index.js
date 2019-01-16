cdda6482f210ee28d39fd1bde9e1f7ead3e81ae9
darrenBlanckensee <darren@entrostat.com>
2019-01-16 16:20:16 +0200

    Mustache js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -15,20 +15,41 @@
 
 
   socket.on('newMessage', function(message) {
-    console.log('new message: ',message);
-    var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
-    jQuery('#messages').append(li);
+
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+      text: message.text,
+      from: message.from,
+      createdAt: new Date().getTime()
+    });
+    jQuery('#messages').append(html);
+
+    // console.log('new message: ',message);
+    // var li = jQuery('<li></li>');
+    // li.text(`${message.from}: ${message.text}`);
+    // jQuery('#messages').append(li);
+
+
   });
 
     socket.on('newLocationMessage', function(message) {
-      var li = jQuery('<li></li>');
-      var a = jQuery('<a target="_blank">My current location</a>');
-      li.text(`${message.from}: `);
-      a.attr('href',message.url);
 
-      li.append(a);
-      jQuery('#messages').append(li);
+      var template = jQuery('#location-message-template').html();
+      var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: new Date().getTime()
+      });
+      jQuery('#messages').append(html);
+
+      //
+      // var li = jQuery('<li></li>');
+      // var a = jQuery('<a target="_blank">My current location</a>');
+      // li.text(`${message.from}: `);
+      // a.attr('href',message.url);
+      //
+      // li.append(a);
+      // jQuery('#messages').append(li);
     })
 
     var messageTextbox = jQuery('[name=message]');
