18470ca22f942a04dfec5fc4a82b0c8af0055e9d
darrenBlanckensee <darren@entrostat.com>
2019-01-16 11:39:24 +0200

    First socket io intro

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 
 publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+  console.log('New User Connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  })
+});
 
 //
 // app.get('/', (req, res) => {
@@ -14,4 +27,4 @@ app.use(express.static(publicPath));
 // });
 
 
-app.listen(port);
+server.listen(port);
