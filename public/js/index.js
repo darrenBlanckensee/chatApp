6551809b6f00397bf2e1566c2f9bd652e4a10c46
darrenBlanckensee <darren@entrostat.com>
2019-01-16 14:34:39 +0200

    Forms and JQuery added

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,9 @@
 
   socket.on('newMessage', function(message) {
     console.log('new message: ',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
   });
 
   socket.emit('createMessage', {
@@ -24,3 +27,13 @@
   }, function(callbackResponse) {
     console.log(callbackResponse);
   });
+
+  jQuery('#message-form').on('submit',function(e) {
+    e.preventDefault();
+    socket.emit('createMessage',{
+      from: 'User',
+      text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+  })
